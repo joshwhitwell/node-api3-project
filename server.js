39c3a26d23454bac14b9cc3dcd7715bf593fbd7c
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-28 15:49:13 -0400

    added logger middleware to server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+//imports
 const express = require('express');
 
+//server
 const server = express();
 
+//middleware
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
+  next()
+}
+
+server.use(express.json())
+server.use(logger)
+
+//routes
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
-function logger(req, res, next) {}
-
+//exports
 module.exports = server;
